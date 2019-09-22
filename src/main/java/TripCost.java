43e94bf7d519bdb5acd76c09afcce11c14e168a4
arakhmatullin <arakhmatullin@cloudally.com>
2019-09-22 23:53:02 +0300

    Init repo

--- src/main/java/TripCost.java
+++ src/main/java/TripCost.java
@@ -1,12 +1,9 @@
 public class TripCost {
-    int getCost(int boardingTaxInRubl, int kilometerTaxInRubl, int tripDistanceInKm) {
-        int ammount = boardingTaxInRubl + kilometerTaxInRubl * tripDistanceInKm;
+    int getCost(int boardingTaxInRuble, int kilometerTaxInRuble, int tripDistanceInKm) {
+        int ammount = boardingTaxInRuble + kilometerTaxInRuble * tripDistanceInKm;
         int discount = 0;
         if (ammount > 1000) {
-            discount = ammount * 5 / 100;
-            if (discount > 100) {
-                discount = 100;
-            }
+            discount = Math.min(ammount * 5 / 100,100);
         }
         return ammount - discount;
     }
