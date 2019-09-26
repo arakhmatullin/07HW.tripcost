7c585868b32cb98247d177f29368abd2b2d8f8d5
arakhmatullin <arakhmatullin@cloudally.com>
2019-09-26 21:45:05 +0300

    Fixes #2 #3

--- src/main/java/TripCost.java
+++ src/main/java/TripCost.java
@@ -1,10 +1,13 @@
 public class TripCost {
     int getCost(int boardingTaxInRuble, int kilometerTaxInRuble, int tripDistanceInKm) {
-        int ammount = boardingTaxInRuble + kilometerTaxInRuble * tripDistanceInKm;
+        int amount = boardingTaxInRuble + kilometerTaxInRuble * tripDistanceInKm;
         int discount = 0;
-        if (ammount > 1000) {
-            discount = Math.min(ammount * 5 / 100,100);
+        int maxDiscountInRuble = 100;
+        int discountInPercent = 5;
+        int minAmountForDiscount = 1000;
+        if (amount > minAmountForDiscount) {
+            discount = Math.min(amount * discountInPercent / 100, maxDiscountInRuble);
         }
-        return ammount - discount;
+        return amount - discount;
     }
 }
